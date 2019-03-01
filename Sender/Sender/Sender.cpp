9819ac94f9bbe618d216cab65d3b9a22f31bb6b1
simondlevy <simon.d.levy@gmail.com>
2019-03-01 20:23:37 +0200

    Sender working

--- Sender/Sender/Sender.cpp
+++ Sender/Sender/Sender.cpp
@@ -1,21 +1,42 @@
-// Sender.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
+
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    VideoCapture cap(0);
+	
+    // VideoWriter: 'videoconvert' converts the 'BGR' images into 'YUY2' raw frames to be fed to
+    // 'jpegenc' encoder since 'jpegenc' does not accept 'BGR' images. The 'videoconvert' is not
+    // in the original pipeline, because in there we are reading frames in 'YUY2' format from 'v4l2src'
+    VideoWriter out("appsrc ! videoconvert ! video/x-raw,format=YUY2,width=640,height=480,framerate=30/1 ! jpegenc ! rtpjpegpay ! udpsink host=127.0.0.1 port=5000",CAP_GSTREAMER,0,30,Size(640,480),true);
+
+	
+    if (!cap.isOpened() | !out.isOpened()) {
+        cerr <<"VideoCapture or VideoWriter not opened"<<endl;
+        exit(-1);
+    }
+
+    while (true) {
+
+        Mat frame;
+
+        cap.read(frame);
+
+        out.write(frame);
+
+        imshow("sender", frame);
+
+        if (waitKey(1) == 27) {
+            break;
+        }
+    }
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
