17d0b5586036acdcfc087d0ed9977bdc1e4a75aa
unknown <simondlevy>
2019-03-03 17:04:50 +0200

    Added JPEG receiver script

---
+++ Windows/h264_receive.bat
@@ -0,0 +1,1 @@
+gst-launch-1.0 -v udpsrc port=5000 ^ ! application/x-rtp, media=video, clock-rate=90000, encoding-name=H264, payload=96 ! rtph264depay ! decodebin ! videoconvert ! fpsdisplaysink video-sink=autovideosink sync=0 silent=true
